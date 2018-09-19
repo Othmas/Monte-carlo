39c06dd73d6f65a51f86f06481fd446ffaa65389
unknown <usrlocal@GA211-FE01.ad.univ-angers.fr>
2018-09-19 10:13:57 +0200

    init commit

--- montecarlo.py
+++ montecarlo.py
@@ -1,5 +1,21 @@
-class Tree(object):
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.data = None
+class Node(object):
+    value = ""
+    children = []
+    
+    def __init__(self, value):
+        self.value = value
+
+    def addChild(self, child):
+        self.children.append(child)
+
+def main():
+    root = Node("root")
+
+    leap1 = Node("leap1")
+    leap2 = Node("leap2")
+
+    root.addChild(leap1)
+    root.addChild(leap2)
+
+main()
+    
